b161134f73f8ec6b735de26d0ffc2b6b7efdc972
Ilya Tkachenko <106911235+detperson@users.noreply.github.com>
2023-03-02 19:46:59 +0300

    Delete index.ts

--- index.ts
+++
@@ -1,9 +0,0 @@
-
-let textArea: HTMLElement = <HTMLElement> document.querySelector('.input-block__input')
-
-textArea.addEventListener("input", function() {
-    textArea.style.height = 'auto'
-    textArea.style.height = (textArea.scrollHeight - 40) + 'px'
-})
-
-
