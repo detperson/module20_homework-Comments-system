0f8e2b99c32fd6892a2d3385b9121d410246afb4
detperson <detperson@gmail.com>
2023-03-09 20:04:59 +0300

    added answers, ratings, sorting, favorites

--- Comment.js
+++ Comment.js
@@ -1,51 +1,169 @@
 export default class Comment {
 
+    constructor(colorRating) {
+        this.colorRating = colorRating
+    }
+
     showComments(comments) {
         let commentField = document.querySelector('.comment-field')
         let out = ''
         comments.forEach(function(item) {
-            out += `
-                <div class="comment">
-                    <div class="comment__user-logo">
-                        <img class="comment__user-logo user-logo-img" src="https://picsum.photos/seed/${item.name}/100" alt="user-logo">
+            if (item.parent === ' ') {
+                out += `
+                    <div class="comment" data-comment-id="${item.id}">
+                        <div class="comment__user-logo">
+                            <img class="comment__user-logo user-logo-img" src="https://picsum.photos/seed/${item.name}/100" alt="user-logo">
+                        </div>
+                        <div class="comment__form">
+                            <div class="comment__other">
+                                <div class="comment__user-name user-name">
+                                    ${item.name}
+                                </div>
+                                <div class="comment__post-data">
+                                    ${timeConverter(item.time)}
+                                </div>
+                            </div>
+                            <div class="comment__text-block">
+                                <p class="comment__body">
+                                    ${item.body}
+                                </p>
+                            </div>
+                            <div class="comment__footer">
+                                <div class="comment__footer-answer footer-btn">
+                                    <img class="comment__footer-answer-img" src="./images/answer_comment.svg" alt="answer">
+                                    <div class="comment__footer-answer-text">Ответить</div>
+                                </div>
+                                <div class="comment__footer-favorites favorites-btn">
+                                    <div class="comment__footer-favorites-logo"></div>
+                                    <div class="comment__footer-favorites-text">В избранное</div>
+                                </div>
+                                <div class="comment__footer-rating">
+                                    <img class="rating-minus-img" src="./images/minus-rating.svg" alt="minus">
+                                    <div class="rating-count">${item.rating}</div>
+                                    <img class="rating-plus-img" src="./images/plus-rating.svg" alt="plus">
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="comment__form">
-                        <div class="comment__other">
-                            <div class="comment__user-name user-name">
-                                ${item.name}
-                            </div>
-                            <div class="comment__post-data">
-                                ${item.time}
-                            </div>
-                        </div>
-                        <div class="comment__text-block">
-                            <p class="comment__body">
-                                ${item.body}
-                            </p>
-                        </div>
-                        <div class="comment__footer">
-                            <div class="comment__footer-answer footer-btn">
-                                <img class="comment__footer-answer-img" src="./images/answer_comment.svg" alt="answer">
-                                <div class="comment__footer-answer-text">Ответить</div>
-                            </div>
-                            <div class="comment__footer-favorites footer-btn">
-                                <img class="comment__footer-favorites-img" src="./images/favorites_comment_off.svg" alt="favorites">
-                                <div class="comment__footer-favorites-text">В избранное</div>
-                            </div>
-                            <div class="comment__footer-rating">
-                                <img class="rating-minus-img" src="./images/minus-rating.svg" alt="minus">
-                                <div class="rating-count">0</div>
-                                <img class="rating-plus-img" src="./images/plus-rating.svg" alt="plus">
-                            </div>
-                        </div>
-                    </div>
-                </div>`
+                    <div class="answers"></div>`
+            }
             // out +=`<p class='comment-date'>${item.time.toLocaleString()}</p>`
             // out +=`<p class='input-block__user-name'>${item.name}</p>`
             // out +=`<p class='comment-body'>${item.body}</p>`
         })
         commentField.innerHTML = out
         this.updateCounterComments()
+
+        // Добавление ответов
+        comments.forEach(function(item) {
+            if (item.parent !== ' ') {
+                //Это цикл для добавления имя родителя ответу.
+                //Его нужно как то упростить, что бы делалось за 1 цикл при добавлении ответа
+                let parentName = ''
+                let parenComment = document.querySelectorAll('.comment')
+                parenComment.forEach(function(com) {
+                    if (item.parent === com.dataset.commentId) {
+                        parentName = com.getElementsByClassName('comment__user-name')[0].innerText
+                    }
+                })
+                //---------------------------------------------------------------------------
+                let outAnswer = `
+                    <div class="comment answer" data-comment-id="${item.id}">
+                        <div class="comment__user-logo">
+                            <img class="comment__user-logo user-logo-img" src="https://picsum.photos/seed/${item.name}/100" alt="user-logo">
+                        </div>
+                        <div class="comment__form">
+                            <div class="comment__other">
+                                <div class="comment__user-name user-name">
+                                    ${item.name}
+                                </div>
+                                <div class="comment__other__answer-to">
+                                    <div class="comment__other__answer-to-logo">
+                                        <img class="comment__other__answer-to-img" src="./images/answer_comment.svg" alt="answer">
+                                    </div>
+                                    <div class="comment__other__answer-to-text">${parentName}</div>
+                                </div>
+                                <div class="comment__post-data">
+                                    ${timeConverter(item.time)}
+                                </div>
+                            </div>
+                            <div class="comment__text-block">
+                                <p class="comment__body">
+                                    ${item.body}
+                                </p>
+                            </div>
+                            <div class="comment__footer">
+                                <div class="answer__footer-favorites favorites-btn">
+                                    <div class="comment__footer-favorites-logo"></div>
+                                    <div class="comment__footer-favorites-text">В избранное</div>
+                                </div>
+                                <div class="comment__footer-rating">
+                                    <img class="rating-minus-img" src="./images/minus-rating.svg" alt="minus">
+                                    <div class="rating-count">${item.rating}</div>
+                                    <img class="rating-plus-img" src="./images/plus-rating.svg" alt="plus">
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+
+                // let parenComment = document.querySelectorAll('.comment')
+                parenComment.forEach(function(com) {
+                    if (item.parent === com.dataset.commentId) {
+                        // parentName = com.getElementsByClassName('comment__user-name')[0].innerText
+                        com.nextElementSibling.insertAdjacentHTML("beforeend", outAnswer)
+                    }
+                })
+
+            }
+        })
+        // Функция конвертирования времени
+        // Не знаю правильно ли ее тут расположил или нет
+        function timeConverter(UNIX_timestamp){
+            let a = new Date(UNIX_timestamp * 1000);
+            let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+            let monthsNumbers = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+            let year = a.getFullYear();
+            let month = monthsNumbers[a.getMonth()];
+            let date = a.getDate();
+            let hour = a.getHours();
+            let min = a.getMinutes();
+            let sec = a.getSeconds();
+            //добавляем нули в начало
+            if (date < 10) {
+                date = '0' + date
+            }
+            if (hour < 10) {
+                hour = '0' + hour
+            }
+            if (min < 10) {
+                min = '0' + min
+            }
+            let time = date + '.' + month + ' ' + hour + ':' + min ;
+            return time;
+        }
+        // let child = `<div class="answer" style="margin-left: 100px">${out}</div>`
+        // let parent = document.querySelectorAll('.comment')
+        // for (let i = 0; i < parent.length; i++) {
+        //     if (parent[i].dataset.commentId === '1') {
+        //         parent[i].insertAdjacentHTML("afterend", child)
+        //     }
+        // }
+
+        this.colorRating()
+        comments.forEach(function(item) {
+            //Добавляем логотип активного "Избранного" и кнопку "В избранном"
+            if (item.inFavorites) {
+                let favoritesBtns = document.querySelectorAll('.comment__footer-favorites-logo')
+                favoritesBtns.forEach(function(favBtn) {
+                    let commentId = favBtn.closest('.comment').dataset.commentId
+                    if (item.id == +commentId) {
+                        favBtn.classList.add('logo-active')
+                        favBtn.nextElementSibling.innerText = 'В избранном'
+                    }
+                })
+            }
+        })
+
     }
 
     //----------------Увеличение-счетчика-сообщений--------------------------------
--- index_copy.js
+++ index_copy.js
@@ -11,7 +11,7 @@ textArea.addEventListener("input", function () {
 
 
 let comments = []
-let com = new Comment()
+let com = new Comment(ratingColor)
 loadComments()
 
 
@@ -19,11 +19,18 @@ loadComments()
 document.querySelector('.btn-send').onclick = function () {
     let commentName = document.getElementById('name-input')
     let commentBody = document.getElementById('textarea1')
+    let parentId = document.getElementById('parent-answer').dataset.parentId
 
     let comment = {
+        id: comments.length + 1,
         name: commentName.value,
         body: commentBody.value,
-        time: timeConverter(Math.floor(Date.now()/1000))
+        // time: timeConverter(Math.floor(Date.now()/1000))
+        time: Math.floor(Date.now()/1000),
+        rating: 0,
+        inFavorites: false,
+        parent: parentId,
+        answers: 0,
     }
 
     commentBody.value = ''
@@ -46,43 +53,6 @@ function loadComments() {
     }
 }
 
-// function showComments() {
-//     let commentField = document.querySelector('.comment-field')
-//     let out = ''
-//     comments.forEach(function(item) {
-//         out += `<div class="comment" style="display: flex">
-//                     <div class="input-block__user-logo">
-//                         <img class="input-block__logo-img" src="https://picsum.photos/seed/logo/100" alt="user-logo">
-//                     </div>
-//                     <div class="input-block__form">
-//                         <div class="input-block__other">
-//                             <div class="input-block__user-name">
-//                                 ${item.name}
-//                             </div>
-//                             <div class="input-block__counter-simbol">
-//                                 ${item.time.toLocaleString()}
-//                             </div>
-//                         </div>
-//                         <div class="input-block__comment-input">
-//                             <p class='comment-body'>${item.body}</p>
-//                         </div>
-//                     </div>
-//                 </div>`
-//         // out +=`<p class='comment-date'>${item.time.toLocaleString()}</p>`
-//         // out +=`<p class='input-block__user-name'>${item.name}</p>`
-//         // out +=`<p class='comment-body'>${item.body}</p>`
-//     })
-//     commentField.innerHTML = out
-// }
-
-//----------------Увеличение-счетчика-сообщений--------------------------------
-
-// function updateCounterComments() {
-//     let counter = document.querySelector('.header-panel__comments-counter')
-//     let comments = JSON.parse(localStorage.getItem('commentss'))
-//     counter.innerText = `(${comments.length})`
-// }
-
 //---------------Количество-введенных-символов-сообщения-----------------------
 
 function counterSymbolMessage() {
@@ -90,39 +60,250 @@ function counterSymbolMessage() {
     let messageLength = textArea.value.length
     let btn = document.querySelector('.input-block__btn-send')
 
+    let messageWarning = document.querySelector('.input-block__warning-long-text')
+
     if (messageLength < 1000) {
         showSymbol.style.color = 'green'
         btn.disabled = false
+        messageWarning.style.opacity = '0'
     } else {
         showSymbol.style.color = '#FF0000'
         btn.disabled = true
+        messageWarning.style.opacity = '1'
     }
     showSymbol.innerText = `${messageLength}/1000`
 }
 
 //--------------Конвертирование-времени----------------------------------------
 
-function timeConverter(UNIX_timestamp){
-    let a = new Date(UNIX_timestamp * 1000);
-    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    let monthsNumbers = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-    let year = a.getFullYear();
-    let month = monthsNumbers[a.getMonth()];
-    let date = a.getDate();
-    let hour = a.getHours();
-    let min = a.getMinutes();
-    let sec = a.getSeconds();
-    //добавляем нули в начало
-    if (date < 10) {
-        date = '0' + date
+// function timeConverter(UNIX_timestamp){
+//     let a = new Date(UNIX_timestamp * 1000);
+//     let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+//     let monthsNumbers = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+//     let year = a.getFullYear();
+//     let month = monthsNumbers[a.getMonth()];
+//     let date = a.getDate();
+//     let hour = a.getHours();
+//     let min = a.getMinutes();
+//     let sec = a.getSeconds();
+//     //добавляем нули в начало
+//     if (date < 10) {
+//         date = '0' + date
+//     }
+//     if (hour < 10) {
+//         hour = '0' + hour
+//     }
+//     if (min < 10) {
+//         min = '0' + min
+//     }
+//     let time = date + '.' + month + ' ' + hour + ':' + min ;
+//     return time;
+// }
+
+//--------------Выпадающий-список-сортировки--------------------------------
+let btnSortList = document.querySelector('.header-panel__sort-text')
+
+btnSortList.addEventListener('click', function() {
+    document.getElementById('dropdownSort').classList.toggle('show')
+})
+
+//Закрывание выпадающего меню когда щелкаем вне его
+window.onclick = function(event) {
+    if (!event.target.matches('.header-panel__sort-text')) {
+        let dropdowns = document.querySelector(".header-panel__dropdown-content");
+        if (dropdowns.classList.contains('show')) {
+            dropdowns.classList.remove('show');
+        }
     }
-    if (hour < 10) {
-        hour = '0' + hour
-    }
-    if (min < 10) {
-        min = '0' + min
-    }
-    let time = date + '.' + month + ' ' + hour + ':' + min ;
-    return time;
 }
 
+//--------------Сортировка-по-дате-----------------------------------------
+
+let btnSortTime = document.querySelector('.sort-time')
+
+btnSortTime.addEventListener('click', function () {
+    if (arrowSort.classList.contains('increase')) {
+        arrowSort.classList.remove('increase')
+    }
+    // sortTime()
+    showSortedComments(byTimeIncrease, byTimeDecrease)
+    document.querySelector('.header-panel__sort-text').dataset.sort = 'time'
+    document.querySelector('.header-panel__sort-text').innerText = btnSortTime.innerText
+})
+
+
+let btnSortRating = document.querySelector('.sort-rating')
+
+btnSortRating.addEventListener('click', function () {
+    if (arrowSort.classList.contains('increase')) {
+        arrowSort.classList.remove('increase')
+    }
+    // sortRating()
+    showSortedComments(byRatingIncrease, byRatingDecrease)
+    document.querySelector('.header-panel__sort-text').dataset.sort = 'rating'
+    document.querySelector('.header-panel__sort-text').innerText = btnSortRating.innerText
+})
+
+
+// Сортировка по возрастанию и убывнию стрелками
+let arrowSort = document.querySelector('.header-panel__sort-arrow')
+
+arrowSort.addEventListener('click', function() {
+    arrowSort.classList.toggle('increase')
+    if (document.querySelector('.header-panel__sort-text').dataset.sort === 'time') {
+        // sortTime()
+        showSortedComments(byTimeIncrease, byTimeDecrease)
+    } else if (document.querySelector('.header-panel__sort-text').dataset.sort === 'rating') {
+        showSortedComments(byRatingIncrease, byRatingDecrease)
+    }
+
+})
+
+
+function showSortedComments(sortedParameterIncrease, sortedParameterDecrease) {
+    if (arrowSort.classList.contains('increase')) {
+        //timeIncrease
+        if (localStorage.getItem('commentss')) {
+            sortedParameterIncrease()
+            // console.log(comments)
+            com.showComments(comments)
+        }
+    } else {
+        //timeDecrease
+        if (localStorage.getItem('commentss')) {
+            sortedParameterDecrease()
+            // console.log(comments)
+            com.showComments(comments)
+        }
+    }
+}
+
+function byTimeIncrease() {
+    comments.sort(function (a, b) {
+        return a.time - b.time
+    })
+}
+
+function byTimeDecrease() {
+    comments.sort(function (a, b) {
+        return b['time'] - a['time']
+    })
+}
+
+function byRatingIncrease() {
+    comments.sort(function (a, b) {
+        return a.rating - b.rating
+    })
+}
+
+function byRatingDecrease() {
+    comments.sort(function (a, b) {
+        return b.rating - a.rating
+    })
+}
+
+//--------------Сортировка-избранного-----------------------------------------
+
+let favoritesList = document.querySelector('.header-panel__favorites')
+
+favoritesList.addEventListener('click', function() {
+    let favoritesArr = []
+    for (let i = 0; i < comments.length; i++){
+        if (comments[i].inFavorites) {
+            favoritesArr.push(comments[i])
+        }
+    }
+    com.showComments(favoritesArr)
+})
+
+//--------------События-при-кликах-на-комментарии--------------------------
+
+let commentField = document.querySelector('.comment-field')
+
+commentField.addEventListener('click', function(event) {
+
+    //Действия для кликов по кнопке избранное
+    if (event.target.classList.contains('comment__footer-favorites-text') || event.target.classList.contains('comment__footer-favorites-logo')) {
+        let commentId = event.target.parentElement.parentElement.parentElement.parentElement.dataset.commentId
+        changeFavorites(commentId)
+        event.target.parentElement.firstElementChild.classList.toggle('logo-active')
+        if (event.target.parentElement.firstElementChild.classList.contains('logo-active')){
+            event.target.parentElement.lastElementChild.innerText = 'В избранном'
+        } else {
+            event.target.parentElement.lastElementChild.innerText = 'В избранное'
+        }
+
+        saveComments()
+    }
+
+    //Действия для кликов по увеличению/уменьшению рейтинга
+    if (event.target.classList.contains('rating-plus-img')) {
+        let commentId = event.target.parentElement.parentElement.parentElement.parentElement.dataset.commentId
+        let counter = event.target.previousElementSibling
+        let res = counter.textContent = `${Number(counter.textContent) + 1}`
+        changeRating(commentId, +res)
+        saveComments()
+        ratingColor()
+    } else if (event.target.classList.contains('rating-minus-img')) {
+        let commentId = event.target.parentElement.parentElement.parentElement.parentElement.dataset.commentId
+        let counterElement = event.target.nextElementSibling
+        let counter = counterElement.textContent = `${Number(counterElement.textContent) - 1}`
+        changeRating(commentId, +counter)
+        saveComments()
+        ratingColor()
+    }
+
+    //Действия для кликов по кнопке ответить
+    if (event.target.classList.contains('comment__footer-answer-text') || event.target.classList.contains('comment__footer-answer-img')) {
+        let commentAuthor = event.target.parentElement.parentElement.previousElementSibling.previousElementSibling.firstElementChild['innerText']
+        let showAnswerName = document.querySelector('.input-block__parent-answer')
+        showAnswerName.style.display = 'flex'
+        showAnswerName.innerHTML =`
+            <div class="input-block__parent-answer-logo">
+                <img class="input-block__parent-answer-img" src="./images/answer_comment.svg" alt="answer">
+            </div>
+            <div class="input-block__parent-answer-text">
+                ${commentAuthor}
+            </div>`
+        let commentId = event.target.parentElement.parentElement.parentElement.parentElement.dataset.commentId
+        document.querySelector('.input-block__parent-answer').dataset.parentId = commentId
+    }
+
+})
+
+function changeFavorites(commentId) {
+    for (let i = 0; i < comments.length; i++) {
+        if (comments[i].id == +commentId) {
+            if (comments[i].inFavorites == false) {
+                comments[i].inFavorites = true
+            } else {
+                comments[i].inFavorites = false
+            }
+        }
+    }
+}
+
+function changeRating(commentId, counter) {
+    for (let i = 0; i < comments.length; i++) {
+        if (comments[i].id == +commentId) {
+            comments[i].rating = counter
+        }
+    }
+}
+
+function ratingColor() {
+    let ratingCounts = document.querySelectorAll('.rating-count')
+    for (let i = 0; i < ratingCounts.length; i++) {
+        if (+ratingCounts[i].textContent > 0) {
+            ratingCounts[i].style.color = '#8AC540'
+        } else if (+ratingCounts[i].textContent < 0) {
+            ratingCounts[i].style.color = '#FF0000'
+        } else if (+ratingCounts[i].textContent == 0) {
+            ratingCounts[i].style.color = 'rgba(0, 0, 0, .4)'
+        }
+    }
+}
+
+function checkCommentAnswer () {
+    document.querySelectorAll('.comment')
+}
